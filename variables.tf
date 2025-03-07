fba67e30a985af3a9a2a1fa89dfad4b639956242
Unnati_v_kohale <kohaleunnati@gmail.com>
2025-03-07 12:58:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,33 @@
-variable project {}
-variable max {}
-variable min {}
-variable desired {}
-variable instance_type {}
+variable "region" {
+  
+}
+variable "project" {
+  
+}
+variable "instance_types" {
+  
+}
+variable "cluster_name" {
+  
+}
+variable "eks_cluster_sub_ids" {
+  
+}
+variable "cluster_role_name" {
+  
+}
+variable "node_role_name" {
+  
+}
+variable "node_group_subnet_ids" {
+  
+}
+variable "desired_size" {
+  
+}
+variable "max_size" {
+  
+}
+variable "min_size" {
+  
+}
